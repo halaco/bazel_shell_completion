17872374fb542771434da1053d2e868d93e96e9f
Sadayuki Kato <sadayuki.kato@gmail.com>
2025-01-23 01:43:54 +0900

    Fix .docker/config.json
    Wrong .docker/config.json is added during the devcontainer creation.
    New part of the post-create.sh fixes this by replace the wrong keyword in config.json.

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,3 +3,10 @@
 
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
+
+# Fix for docker config.json.
+docker_config="$HOME/.docker/config.json"
+
+if [ -f "$docker_config" ]; then
+    sed -i s/credsStore/credStore/g $docker_config
+fi
